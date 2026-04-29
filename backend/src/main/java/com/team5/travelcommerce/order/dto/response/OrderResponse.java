ffe64082dd05fcd3fb02ce269d269f0cec55a792
CHW0n <chw9285@gmail.com>
2026-04-29 16:17:33 +0900

    Update OrderResponse.java

--- backend/src/main/java/com/team5/travelcommerce/order/dto/response/OrderResponse.java
+++ backend/src/main/java/com/team5/travelcommerce/order/dto/response/OrderResponse.java
@@ -11,20 +11,43 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class OrderResponse {
+
     private String id;
+    private Long productId;
+    private Long productDateId;
+
     private String title;
     private String dateText;
     private int people;
+    private int unitPrice;
     private int totalPrice;
     private String productImageUrl;
+
+    private String travelerFirstName;
+    private String travelerLastName;
+    private String travelerPhone;
+    private String travelerEmail;
+
+    private String orderStatus;
+    private String createdAt;
+
     public static OrderResponse from(Order order) {
         return OrderResponse.builder()
                 .id(order.getId().toString())
+                .productId(order.getProduct().getId())
+                .productDateId(order.getProductDate().getId())
                 .title(order.getProductTitleSnapshot())
                 .dateText(order.getUseDate().toString())
                 .people(order.getPeople())
+                .unitPrice(order.getUnitPrice())
                 .totalPrice(order.getTotalPrice())
                 .productImageUrl(order.getProductImageSnapshot())
+                .travelerFirstName(order.getTravelerFirstName())
+                .travelerLastName(order.getTravelerLastName())
+                .travelerPhone(order.getTravelerPhone())
+                .travelerEmail(order.getTravelerEmail())
+                .orderStatus(order.getOrderStatus().name())
+                .createdAt(order.getOrderedAt().toString())
                 .build();
     }
 }
