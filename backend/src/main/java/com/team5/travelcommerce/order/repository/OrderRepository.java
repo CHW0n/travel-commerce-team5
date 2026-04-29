c6dd14c3141877da80874ba3046aed07f2d1eacc
CHW0n <chw9285@gmail.com>
2026-04-29 15:37:51 +0900

    Update OrderRepository.java

--- backend/src/main/java/com/team5/travelcommerce/order/repository/OrderRepository.java
+++ backend/src/main/java/com/team5/travelcommerce/order/repository/OrderRepository.java
@@ -3,5 +3,9 @@ package com.team5.travelcommerce.order.repository;
 import com.team5.travelcommerce.order.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    List<Order> findByUserId(Long userId);
 }
